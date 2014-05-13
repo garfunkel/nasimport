72a8965b98e16384f4bb51f1678d3c76791b6626
Simon Allen <its.simon.a@gmail.com>
2014-05-13 21:42:23 +1000

    remove old constants module

--- constants/constants.go
+++
@@ -1,8 +0,0 @@
-package constants
-
-const (
-	MediaRoot = "/home/simon/dataFamily/"
-	TVRoot = MediaRoot + "TV"
-	DocumentaryRoot = MediaRoot + "Documentaries"
-	MovieRoot = MediaRoot + "Movies"
-)
